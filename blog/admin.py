1b7f50aebca92c9972104ae49c0304b29b07332c
anusia107d <anusia107d@wp.pl>
2016-07-30 16:08:43 +0200

    Second commit

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
+# rejestracja istniejących w pliku models.py modeli
 admin.site.register(Post)
+admin.site.register(Comment)
